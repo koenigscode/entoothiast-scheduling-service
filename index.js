328c9a1f3daa463e9b35183dc5e75837f8d43d0a
Anna <annamak@student.chalmers.se>
2023-11-29 13:54:42 +0100

    Add clinic functionality

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import * as mqtt from "mqtt"
 import MqttRequest from "mqtt-request"
+import PGClient from "pg-native"
 
 const client = mqtt.connect(process.env.BROKER_URL)
 /** @type {MqttRequest}*/
 const mqttReq = new MqttRequest.default(client);
 
 console.log(`Broker URL: ${process.env.BROKER_URL}`)
+const db = new PGClient()
+db.connectSync(process.env.CONNECTION_STRING)
 
 mqttReq.response("demo", payload => {
     payload = JSON.parse(payload)
@@ -14,7 +17,18 @@ mqttReq.response("demo", payload => {
     return JSON.stringify(payload)
 })
 
+mqttReq.response("v1/clinics/read", (payload) => {
+    payload = JSON.parse(payload)
+    console.log(payload)
+
+    try {
+        const clinics = db.querySync('SELECT * FROM public.clinic')
+        return JSON.stringify({ httpStatus: 200, clinics})
+    } catch (e) {
+        return JSON.stringify({ httpStatus: 400, message: "No clinics found"})
+    }
+})
+
 client.on("connect", () => {
     console.log("scheduling-service connected to broker")
 });
-
