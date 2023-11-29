f3adfba874725a37a75e265ebeb30fe955fe3a1e
Milena Maćkowiak <milenam@student.chalmers.se>
2023-11-29 11:33:09 +0100

    Functionality for GET all timeslots route

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import * as mqtt from "mqtt"
 import MqttRequest from "mqtt-request"
+import PGClient from "pg-native"
+
+const db = new PGClient()
+db.connectSync(process.env.CONNECTION_STRING)
 
 const client = mqtt.connect(process.env.BROKER_URL)
 /** @type {MqttRequest}*/
@@ -14,7 +18,22 @@ mqttReq.response("demo", payload => {
     return JSON.stringify(payload)
 })
 
+mqttReq.response("v1/timeslots", (payload) => {
+    
+    payload = JSON.parse(payload);
+    console.log(payload)
+
+    if (!payload.startTime)
+        return JSON.stringify({ httpStatus: 400, message: "Start time needs to be specified." })
+
+    try {
+        db.querySync("select from public.timeslot where start_time = $1", [payload.startTime || Date.now()])
+        return JSON.stringify({ httpStatus: 201, message: `${payload}` })
+    } catch (e) {
+        return JSON.stringify({ httpStatus: 400, message: "Timeslots with this start time not found."})
+    }
+});
+
 client.on("connect", () => {
     console.log("scheduling-service connected to broker")
 });
-
