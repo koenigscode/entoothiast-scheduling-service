f1302ccd1c7a52bff1217aee39c761d7e70f4fdc
serbina <serbina@student.chalmers.se>
2023-12-23 15:25:03 +0300

    Fix db queries #3

--- index.js
+++ index.js
@@ -39,11 +39,12 @@ mqttReq.response("v1/dentists/read", (payload) => {
 });
 
 mqttReq.response("v1/timeslots/delete", (payload) => {
+    //change the query
     payload = JSON.parse(payload)
 
     try {
         const result = db.querySync(
-            `DELETE FROM public.timeslot WHERE id = ${payload.timeslotId} AND dentist_id = ${payload.dentistId} RETURNING *`
+            'DELETE FROM public.timeslot WHERE id = $1 AND dentist_id = $2 RETURNING *', [payload.timeslotId, payload.dentistId]
         );
 
         if (result && result.length > 0) {
@@ -76,13 +77,13 @@ mqttReq.response("v1/dentists/ratings/create", (payload) => {
         //don't forget to also send the patient_id -> this table has a compound key of patient_id and dentist_id
         const token = jwt.decode(payload.token)
         console.log(token)
-        const dentist = db.querySync(`SELECT * FROM public.patient_on_dentist WHERE dentist_id = ${payload.dentistId}`);
+        const dentist = db.querySync(`SELECT * FROM public.patient_on_dentist WHERE dentist_id = $1`, [payload.dentistId]);
         if (dentist.length === 0){
             db.querySync("insert into public.patient_on_dentist (patient_id, dentist_id, rating) values ($1, $2, $3)", [token.id, payload.dentistId, rating])
         }
         //if there is a dentist in the dentist_on_patient table, just insert a new rating.
         else{
-             db.querySync(`UPDATE public.patient_on_dentist SET rating = ${payload.rating} where dentist_id = ${payload.dentistId}`)
+             db.querySync(`UPDATE public.patient_on_dentist SET rating = $1 where dentist_id = $2`, [payload.rating, payload.dentistId])
         }
         return JSON.stringify({ httpStatus: 201, message: `Posted new rating of ${rating} for dentist with id ${payload.dentistId}`})
     } catch (e) {
