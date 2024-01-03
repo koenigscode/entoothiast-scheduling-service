d43fc89fe75aec267376559a4bd52c61e1a5e5f8
Michael Koenig <koenigmichael2002@gmail.com>
2024-01-03 18:27:51 +0200

    Use shared mqtt subscriptions

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ import { createTimeslot, deleteTimeslot } from "./controllers/v1/timeslots.js"
 const client = mqtt.connect(process.env.BROKER_URL)
 
 MqttRequest.timeout = 5000;
+MqttRequest.publishOptions = { qos: 2 }
 
 /** @type {MqttRequest}*/
 export const mqttReq = new MqttRequest.default(client);
 
 console.log(`Broker URL: ${process.env.BROKER_URL}`)
 
-mqttReq.response("v1/dentists/read", readDentists);
-mqttReq.response("v1/dentists/ratings/create", rateDentist);
+mqttReq.response("$share/scheduling-service/v1/dentists/read", readDentists);
+mqttReq.response("$share/scheduling-service/v1/dentists/ratings/create", rateDentist);
 
-mqttReq.response("v1/timeslots/delete", deleteTimeslot);
-mqttReq.response("v1/timeslots/create", createTimeslot);
+mqttReq.response("$share/scheduling-service/v1/timeslots/delete", deleteTimeslot);
+mqttReq.response("$share/scheduling-service/v1/timeslots/create", createTimeslot);
 
-mqttReq.response("v1/appointments/all", allAppointments);
-mqttReq.response("v1/appointments/read", readAppointment);
-mqttReq.response("v1/appointments/create", createAppointment);
-mqttReq.response("v1/appointments/update", updateAppointment);
+mqttReq.response("$share/scheduling-service/v1/appointments/all", allAppointments);
+mqttReq.response("$share/scheduling-service/v1/appointments/read", readAppointment);
+mqttReq.response("$share/scheduling-service/v1/appointments/create", createAppointment);
+mqttReq.response("$share/scheduling-service/v1/appointments/update", updateAppointment);
 
-mqttReq.response("v1/clinics/read", readClinics)
-mqttReq.response("v1/clinics/create", createClinic);
-mqttReq.response("v1/clinics/update", updateClinic);
-mqttReq.response("v1/clinics/delete", deleteClinic);
+mqttReq.response("$share/scheduling-service/v1/clinics/read", readClinics)
+mqttReq.response("$share/scheduling-service/v1/clinics/create", createClinic);
+mqttReq.response("$share/scheduling-service/v1/clinics/update", updateClinic);
+mqttReq.response("$share/scheduling-service/v1/clinics/delete", deleteClinic);
 
 
 client.on("connect", () => {
